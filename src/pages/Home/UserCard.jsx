2f527ecb4bdf55a0ee4e5daf94d69a0ccd2ec11c
Rodrigo Oliveira <rdgsec@gmail.com>
2021-04-18 15:38:40 -0300

    add styles to user card

--- src/pages/Home/UserCard.jsx
+++ src/pages/Home/UserCard.jsx
@@ -12,22 +12,31 @@ function UserCard({ user }) {
   }
 
   return (
-    <div>
-      <h1>{user.name}</h1>
-      <p>
-        {`${user.login}`}
-      </p>
-      <p>{user.bio}</p>
-      <p>
-        followers:
-        {user.followers}
-      </p>
-      <p>
-        following:
-        {user.following}
-      </p>
-      <div>
-        <Button type="button" onClick={handleDetailsClick}>+ Details</Button>
+    <div className="c-card">
+      <img className="c-card__avatar" src={user.avatar_url} alt="User profile" />
+      <section className="c-card__info">
+        <h1 className="c-card__title">{user.name}</h1>
+        <p className="c-card__subtitle">{`${user.login}`}</p>
+        <div className="c-card__content">
+          <p>{user.bio}</p>
+          <div className="c-badges">
+            <div className="c-badges__item c-badges__item--dark">
+              <span className="c-badges__text c-badges__text--bold c-badges__text--white">{user.followers}</span>
+              <span className="c-badges__text">followers</span>
+            </div>
+            <div className="c-badges__item c-badges__item--dark">
+              <span className="c-badges__text c-badges__text--bold c-badges__text--white">
+                {user.following}
+              </span>
+              <span className="c-badges__text">following</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="c-card__actions">
+        <Button className="c-button" type="button" onClick={handleDetailsClick}>
+          + Details
+        </Button>
       </div>
     </div>
   );
