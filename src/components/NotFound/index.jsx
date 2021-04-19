b16f426cb9d14c7d44450e3dc410e2c14ca58978
Rodrigo Oliveira <rdgsec@gmail.com>
2021-04-18 22:45:10 -0300

    add fonts and notfound component

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function NotFound() {
-  return <div>Content Not Found</div>;
+  return (
+    <section className="c-notfound">
+      <h1 className="c-notfound__title">We couldn&apos;t find anything</h1>
+      <p className="c-notfound__message">Try another username.</p>
+    </section>
+  );
 }
 
 export { NotFound };
