3fa8f042df16c1c13ed7436d6361df667d7b3682
Rodrigo Oliveira <rdgsec@gmail.com>
2021-04-19 09:24:01 -0300

    add landing message

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,9 +5,13 @@ import SearchUserResult from './SearchUserResult';
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <section className="c-landing">
+        <h1 className="c-landing__title">Search over 56 million of developers</h1>
+        <p className="c-landing__subtitle">GitHub is the largest and most advanced development platform in the world</p>
+      </section>
       <SearchForm />
       <SearchUserResult />
-    </div>
+    </>
   );
 }
