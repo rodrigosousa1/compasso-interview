411c25fc190e57b12fb1cc5bfad116c5298535fc
Rodrigo Oliveira <rdgsec@gmail.com>
2021-04-19 15:10:52 -0300

    fix landing text

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <section className="c-landing">
-        <h1 className="c-landing__title">Search over 56 million of developers</h1>
+        <h1 className="c-landing__title">Search over 56 million developers</h1>
         <p className="c-landing__subtitle">GitHub is the largest and most advanced development platform in the world</p>
       </section>
       <SearchForm />
