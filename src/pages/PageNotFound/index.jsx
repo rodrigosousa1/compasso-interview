30b6780197983e79137c32490a81b62721abd799
Rodrigo Oliveira <rdgsec@gmail.com>
2021-04-18 21:25:17 -0300

    add notfound page styles

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Button } from '../../components';
+import { clearUserState } from '../../redux/modules/users';
+
 export default function PageNotFound() {
-  return <div>404 - Page Not Found</div>;
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    dispatch(clearUserState());
+    history.push('/');
+  }
+
+  return (
+    <section className="c-notfound">
+      <h1 className="c-notfound__code">404</h1>
+      <p className="c-notfound__title">Something&apos;s missing</p>
+      <p className="c-notfound__message">The page you looking is not found.</p>
+      <Button className="c-button" type="button" onClick={handleClick}>Go to homepage</Button>
+    </section>
+  );
 }
